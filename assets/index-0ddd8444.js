e110a53f67d5b6cabbf8311937d8ea0740c2f418
bf_tsai <tsai19911026@gmail.com>
2023-08-24 02:07:32 +0800

    Updates

---
+++ assets/index-0ddd8444.js
@@ -0,0 +1,1 @@
+import"./bootstrap.bundle-95fc84b3.js";const o=document.querySelector(".blueBox");gsap.fromTo(o,{opacity:0,x:0,y:0,duration:3,rotste:0,backgroundColor:"yellow"},{opacity:1,x:300,y:300,duration:3,rotate:180,backgroundColor:"pink"});
--- assets/index-8775d5fa.js
+++
@@ -1,1 +0,0 @@
-import"./bootstrap.bundle-5b4687e5.js";console.log("Hello");
